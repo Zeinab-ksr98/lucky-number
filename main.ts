578afb565603aa7c697802083a452820207bf3b8
Zeinab kassir <87263087+Zeinab-ksr98@users.noreply.github.com>
2025-07-25 11:24:58 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+let random = 0
+input.onButtonPressed(Button.B, function () {
+    random = randint(0, 5)
+    basic.showString("lucky number")
+    if (random == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+    }
+    if (random == 2) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . # . . .
+            . # # # .
+            `)
+    }
+    if (random == 3) {
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    }
+    if (random == 4) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # # # .
+            . . . # .
+            . . . # .
+            `)
+    }
+    if (random == 5) {
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
+            . # # # .
+            `)
+    }
+})
